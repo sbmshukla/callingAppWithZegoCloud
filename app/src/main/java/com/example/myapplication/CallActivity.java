b95be9d01b8666b31705d05bb7e073c673b0f67c
sbmshukla <sbmshukla17@gmail.com>
2023-04-19 19:08:17 +0530

    Working On Forwarding Services

--- app/src/main/java/com/example/myapplication/CallActivity.java
+++ app/src/main/java/com/example/myapplication/CallActivity.java
@@ -1,31 +1,43 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.zegocloud.uikit.prebuilt.call.invite.widget.ZegoSendCallInvitationButton;
 import com.zegocloud.uikit.service.defines.ZegoUIKitUser;
 
-import java.util.Collection;
 import java.util.Collections;
 
 public class CallActivity extends AppCompatActivity {
 
-    ZegoSendCallInvitationButton voiceCall,videoCall;
+    ZegoSendCallInvitationButton voiceCall, videoCall;
     EditText receiver;
     String userId;
+    String targetUserId;
+    Button button2;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call);
-        receiver=findViewById(R.id.receiver);
-        voiceCall=findViewById(R.id.voiceCall);
-        videoCall=findViewById(R.id.videoCall);
-        userId=getIntent().getStringExtra("userId");
+        receiver = findViewById(R.id.receiver);
+        voiceCall = findViewById(R.id.voiceCall);
+        videoCall = findViewById(R.id.videoCall);
+        button2 = findViewById(R.id.button2);
+
+        userId = getIntent().getStringExtra("userId");
+        targetUserId = receiver.getText().toString().trim();
         receiver.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -34,9 +46,21 @@ public class CallActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                String targetUserId=receiver.getText().toString().trim();
-                setVoiceCall(targetUserId);
-                setVideoCall(targetUserId);
+                targetUserId = receiver.getText().toString().trim();
+                if (userExist(targetUserId)) {
+                    setVoiceCall(targetUserId);
+                    setVideoCall(targetUserId);
+                } else {
+
+                    button2.setOnClickListener(v -> {
+
+                        Intent I = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + targetUserId));
+                        startActivity(I);
+
+                    });
+                }
+
+
             }
 
             @Override
@@ -48,14 +72,22 @@ public class CallActivity extends AppCompatActivity {
 
     }
 
+    private boolean userExist(String targetUserId) {
+        return false;
+    }
+
+
     public void setVoiceCall(String targetUserId) {
         voiceCall.setIsVideoCall(false);
         voiceCall.setResourceID("zego_uikit_call");
         voiceCall.setInvitees(Collections.singletonList(new ZegoUIKitUser(targetUserId)));
+
     }
-    public void setVideoCall(String targetUserId){
+
+    public void setVideoCall(String targetUserId) {
         videoCall.setIsVideoCall(true);
         videoCall.setResourceID("zego_uikit_call");
         videoCall.setInvitees(Collections.singletonList(new ZegoUIKitUser(targetUserId)));
     }
+
 }
--- app/src/main/java/com/example/myapplication/MainScreen.java
+++ app/src/main/java/com/example/myapplication/MainScreen.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Application;
@@ -7,16 +8,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.ktx.Firebase;
 import com.zegocloud.uikit.prebuilt.call.config.ZegoNotificationConfig;
 import com.zegocloud.uikit.prebuilt.call.invite.ZegoUIKitPrebuiltCallInvitationConfig;
 import com.zegocloud.uikit.prebuilt.call.invite.ZegoUIKitPrebuiltCallInvitationService;
+
+import java.util.ArrayList;
+
+import im.zego.zegoexpress.ZegoExpressEngine;
+import im.zego.zegoexpress.callback.IZegoEventHandler;
+import im.zego.zegoexpress.constants.ZegoAudioCaptureStereoMode;
+import im.zego.zegoexpress.constants.ZegoAudioConfigPreset;
+import im.zego.zegoexpress.constants.ZegoScenario;
+import im.zego.zegoexpress.entity.ZegoAudioConfig;
+import im.zego.zegoexpress.entity.ZegoEngineProfile;
 
 public class MainScreen extends AppCompatActivity {
 
     EditText userId;
     Button startButton;
     String userIdStr;
+    ArrayList<UserModel> userModels=new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +46,14 @@ public class MainScreen extends AppCompatActivity {
         userId=findViewById(R.id.editTextText);
         startButton=findViewById(R.id.button);
 
+
         startButton.setOnClickListener(v -> {
             userIdStr=userId.getText().toString();
             if (userIdStr.isEmpty()){
                 return;
             }
             startService(userIdStr);
+
             Intent i=new Intent(MainScreen.this,CallActivity.class);
             i.putExtra("userId",userIdStr);
             startActivity(i);
@@ -50,7 +73,13 @@ public class MainScreen extends AppCompatActivity {
         notificationConfig.sound = "zego_uikit_sound_call";
         notificationConfig.channelID = "CallInvitation";
         notificationConfig.channelName = "CallInvitation";
+
+
+
         ZegoUIKitPrebuiltCallInvitationService.init(application, appID, appSign, userID, userName,callInvitationConfig);
+
+
+
 
     }
 
